71c3353568eceb8c37b2311c63e64cfc33f473a8
Nayara Costa <nayara.cristinarc@gmail.com>
2023-07-27 23:55:29 -0300

    Projeto CRUD Emprestimo de Livros

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,105 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+
+        public EmprestimoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+
+            return View(emprestimos);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar() 
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id) 
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimos = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimos == null) 
+            {
+                return NotFound();
+            }
+
+            return View(emprestimos);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimos = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimos == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimos);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Emprestimos.Add(emprestimos);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Update(emprestimo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Emprestimos.Remove(emprestimo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
